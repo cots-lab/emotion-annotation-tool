fb3320e01d3f7c7a79295505b77cef62fdf78a1a
TushanAthukorala <2020is008@stu.ucsc.cmb.ac.lk>
2024-01-22 13:14:30 +0530

    add relaxation and track selection

--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,9 @@ const mainController = require("../controllers/main");
 const router = express.Router();
 
 router.get("/", mainController.getMain);
-router.post("/", mainController.postMain);
 
+//COTS Tool
+router.get("/cots-instruction", mainController.postMain);
 router.get("/help", mainController.getHelp);
 router.get("/annotation", mainController.getAnnotation);
 router.get("/arousal-annotation", mainController.getArousalAnnotation);
@@ -13,4 +14,14 @@ router.get("/valence-annotation", mainController.getValenceAnnotation);
 router.get("/additional-annotation", mainController.getAdditionalAnnotations);
 router.get("/form-end", mainController.getFromEnd);
 router.get("/doserial", mainController.doSerial);
+
+//SAM Tool
+router.post("/", mainController.samPostMain);
+router.get("/sam-help", mainController.samGetHelp);
+router.get("/sam-annotation", mainController.samGetAnnotation);
+router.get("/sam-arousal-annotation", mainController.samGetArousalAnnotation);
+router.get("/sam-valence-annotation", mainController.samGetValenceAnnotation);
+router.get("/sam-additional-annotation", mainController.samGetAdditionalAnnotations);
+router.get("/sam-form-end", mainController.samGetFromEnd);
+
 module.exports = router;
